484a92f806275425b3a28638c23b8ef176c24971
GiacomoMadrid <gsmr.18@gmail.com>
2025-05-24 10:15:46 -0500

    .env

--- sources/backend/backend/settings.py
+++ sources/backend/backend/settings.py
@@ -72,10 +72,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#Database Postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'potia',
+        'USER': 'postgres',
+        'PASSWORD': 'virtualmiau16', # ←  Contraseña
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
